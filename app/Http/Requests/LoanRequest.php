07418fc03ace866ff33f52b5774e514ecf073d51
khiemhome <khiembi161219@gmail.com>
2024-09-09 15:34:50 +0700

    change table loan

--- app/Http/Requests/LoanRequest.php
+++ app/Http/Requests/LoanRequest.php
@@ -23,15 +23,61 @@ class LoanRequest extends FormRequest
      */
     public function rules(): array
     {
-        return [
+//        return [
+//            'customer_id' => 'required',
+//            'package_id' => 'required',
+//            'payment_period' => 'required',
+//            'document_type' => 'required',
+//            'nid_driver_license_number' => 'required|numeric',
+//            'nid_driver_license_file' => 'required|file',
+//            'work_id_number' => 'required|numeric',
+//            'work_id_file' => 'required|file',
+//            'selfie' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+//            'pay_slip_1' => 'required|file',
+//            'pay_slip_2' => 'required|file',
+//            'pay_slip_3' => 'required|file',
+//            'fn_pay_amount_1' => 'nullable|numeric',
+//            'fn_pay_amount_2' => 'nullable|numeric',
+//            'fn_pay_amount_3' => 'nullable|numeric',
+//            'next_fn_pay' => 'nullable|date',
+//        ];
+
+        $rules = [
             'customer_id' => 'required',
             'package_id' => 'required',
-            'start_date' => 'required|date',
-            'repayment_date' => 'required|date',
-            'amount' => 'required',
-            'paid' => 'required',
-            'status' => 'required',
+            'payment_period' => 'required',
+            'document_type' => 'required',
+            'nid_driver_license_number' => 'required|numeric',
+//            'nid_driver_license_file' => 'required|file',
+            'work_id_number' => 'required|numeric',
+//            'work_id_file' => 'required|file',
+//            'selfie' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+//            'pay_slip_1' => 'required|file',
+//            'pay_slip_2' => 'required|file',
+//            'pay_slip_3' => 'required|file',
+            'fn_pay_amount_1' => 'nullable|numeric',
+            'fn_pay_amount_2' => 'nullable|numeric',
+            'fn_pay_amount_3' => 'nullable|numeric',
+            'next_fn_pay' => 'nullable|date',
         ];
+
+        if ($this->method() === 'PUT' || $this->method() === 'PATCH') {
+            $rules['nid_driver_license_file'] = 'nullable|file';
+            $rules['work_id_file'] = 'nullable|file';
+            $rules['selfie'] = 'nullable|file';
+            $rules['pay_slip_1'] = 'nullable|file';
+            $rules['pay_slip_2'] = 'nullable|file';
+            $rules['pay_slip_3'] = 'nullable|file';
+        } else {
+            $rules['nid_driver_license_file'] = 'required|file';
+            $rules['work_id_file'] = 'required|file';
+            $rules['selfie'] = 'required|file';
+            $rules['pay_slip_1'] = 'required|file';
+            $rules['pay_slip_2'] = 'required|file';
+            $rules['pay_slip_3'] = 'required|file';
+        }
+
+        return $rules;
     }
 
     protected function failedValidation(Validator $validator)
--- app/Models/Loan.php
+++ app/Models/Loan.php
@@ -9,8 +9,25 @@ class Loan extends Model
 {
     use HasFactory;
 
-    protected $table = 'customer_package';
-    protected $fillable = ['customer_id', 'package_id', 'start_date', 'repayment_date', 'amount', 'paid', 'status'];
+    protected $table = 'loans';
+    protected $fillable = [
+        'customer_id',
+        'document_type',
+        'nid_driver_license_number',
+        'nid_driver_license_file',
+        'work_id_number',
+        'work_id_file',
+        'selfie',
+        'package_id',
+        'payment_period',
+        'pay_slip_1',
+        'pay_slip_2',
+        'pay_slip_3',
+        'fn_pay_amount_1',
+        'fn_pay_amount_2',
+        'fn_pay_amount_3',
+        'next_fn_pay'
+    ];
 
     public function customer()
     {
@@ -20,5 +37,10 @@ class Loan extends Model
     public function package()
     {
         return $this->belongsTo(Package::class, 'package_id');
+    }
+
+    public function paymentPeriod()
+    {
+        return $this->belongsTo(PaymentPeriod::class, 'payment_period');
     }
 }

3253c84c0824e5200ce44a8db1be294f632b15f5
khiemhome <khiembi161219@gmail.com>
2024-10-28 09:54:54 +0700

    data seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,7 @@ use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,21 +17,52 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-//        User::factory()->create([
-//            'name' => 'Test User',
-//            'email' => 'test@example.com',
-//        ]);
+        User::factory()->create([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('admin@gmail.com'),
+        ]);
 
         DB::table('packages')->insert([
-            ['title' => 'K 100', 'amount' => 100],
-            ['title' => 'K 500', 'amount' => 500],
-            ['title' => 'K 1000', 'amount' => 1000],
+            [
+                'title' => 'K 100',
+                'amount' => 100,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'title' => 'K 500',
+                'amount' => 500,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'title' => 'K 1000',
+                'amount' => 1000,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
         ]);
 
         DB::table('payment_periods')->insert([
-            ['title' => '1 FN (25%)', 'amount' => 25],
-            ['title' => '2 FN (50%)', 'amount' => 50],
-            ['title' => '3 FN (75%)', 'amount' => 75],
+            [
+                'title' => 'FN (25%)',
+                'percent' => 25,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'title' => 'FN (50%)',
+                'percent' => 50,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'title' => 'FN (75%)',
+                'percent' => 75,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
         ]);
     }
 }
